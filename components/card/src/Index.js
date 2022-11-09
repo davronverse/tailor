7b0fb6298dcd9ca4c5c2c11f27cf10c281a044cb
devA717 <sweetjustin0717@gmail.com>
2022-11-09 07:52:27 -0800

    card bug fix

--- components/card/src/Index.js
+++ components/card/src/Index.js
@@ -27,39 +27,39 @@ const Card = {
 
 };
 
-$('.blog-card').on({
-    mousedown: function () {
-        if(cardClicked == false) {
-          $('.blog-card-image').addClass('hidden');
-          $('.blog-card-content').addClass("bg-[url('../../dist/img/card/card1.jpg')]");
-          $('.blog-card-content').addClass("bg-cover");
-          $('.blog-card-content').addClass("rounded-lg");
+// $('.blog-card').on({
+//     mousedown: function () {
+//         if(cardClicked == false) {
+//           $('.blog-card-image').addClass('hidden');
+//           $('.blog-card-content').addClass("bg-[url('../../dist/img/card/card1.jpg')]");
+//           $('.blog-card-content').addClass("bg-cover");
+//           $('.blog-card-content').addClass("rounded-lg");
 
-          $('.blog-card-content').addClass("blog-card-clicked");
+//           $('.blog-card-content').addClass("blog-card-clicked");
 
-          $('.blog-card-title').addClass("text-purple-100");
-          $('.blog-card-text').addClass("text-purple-100");
-          $('.blog-card-status').addClass("text-purple-100");
-          $('.blog-card-btn').addClass("text-purple-100");
-          //$('.blog-card-content').css("height": "428px");
+//           $('.blog-card-title').addClass("text-purple-100");
+//           $('.blog-card-text').addClass("text-purple-100");
+//           $('.blog-card-status').addClass("text-purple-100");
+//           $('.blog-card-btn').addClass("text-purple-100");
+//           //$('.blog-card-content').css("height": "428px");
 
-          cardClicked = true;
-        } else {
-          $('.blog-card-image').removeClass('hidden');
-          $('.blog-card-content').removeClass("bg-[url('../../dist/img/card/card1.jpg')]");
-          $('.blog-card-content').removeClass("bg-cover");
-          $('.blog-card-content').removeClass("rounded-lg");
+//           cardClicked = true;
+//         } else {
+//           $('.blog-card-image').removeClass('hidden');
+//           $('.blog-card-content').removeClass("bg-[url('../../dist/img/card/card1.jpg')]");
+//           $('.blog-card-content').removeClass("bg-cover");
+//           $('.blog-card-content').removeClass("rounded-lg");
 
-          $('.blog-card-title').removeClass("text-purple-100");
-          $('.blog-card-text').removeClass("text-purple-100");
-          $('.blog-card-status').removeClass("text-purple-100");
-          $('.blog-card-btn').removeClass("text-purple-100");
+//           $('.blog-card-title').removeClass("text-purple-100");
+//           $('.blog-card-text').removeClass("text-purple-100");
+//           $('.blog-card-status').removeClass("text-purple-100");
+//           $('.blog-card-btn').removeClass("text-purple-100");
 
-          $('.blog-card-content').removeClass("blog-card-clicked");
-          cardClicked = false;
-        }
-    }
-});
+//           $('.blog-card-content').removeClass("blog-card-clicked");
+//           cardClicked = false;
+//         }
+//     }
+// });
 
 // export
 export default Card;
