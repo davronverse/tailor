aa792fbd18ad40e7d71b4acc23ccbe21f30863b0
devA717 <sweetjustin0717@gmail.com>
2022-11-09 00:09:34 -0800

    image scale grow

---
+++ components/Buttons/js/index.js
@@ -0,0 +1,17 @@
+// import jQuery
+import $ from 'jquery';
+
+// import flowbite
+import flowbite from 'flowbite';
+
+// Import Swiper and modules
+// https://swiperjs.com/swiper-api#using-js-modules
+import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
+
+
+
+$('.blog-card inline-block').on({
+    mousedown: function () {
+        alert(1)
+    }
+});
---
+++ components/Buttons/src/Index.js
@@ -0,0 +1,31 @@
+// import jQuery
+import $ from 'jquery';
+
+// import flowbite
+import flowbite from 'flowbite';
+
+// Import Swiper and modules
+// https://swiperjs.com/swiper-api#using-js-modules
+import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
+
+
+/**
+ * Function Description Goes Here
+ */
+const Footer = {
+
+  init() {
+    console.log('hit');
+  },
+
+  methodName($param) {
+
+  },
+
+};
+
+// export
+export default Footer;
+
+// initiate
+Footer.init();
