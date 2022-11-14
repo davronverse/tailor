e9710d97362f63eb1af875b0a3e0dffc4855a309
justinM717 <sweetjustin0717@gmail.com>
2022-11-15 04:44:21 +0900

    propertycard update

--- components/propertycard/src/Index.js
+++ components/propertycard/src/Index.js
@@ -12,10 +12,10 @@ import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
 /**
  * Function Description Goes Here
  */
-const Footer = {
+const PropertyCard = {
 
   init() {
-    console.log('hit');
+    console.log('property card');
   },
 
   methodName($param) {
@@ -23,9 +23,41 @@ const Footer = {
   },
 
 };
+$(function () {
+  let width = window.innerWidth;
+  if (width <= 648) {
+    $("#popover-additional-btn-h").hide();
+  } else {
+    $("#popover-additional-btn-c").hide();
+  }
+});
+
+window.addEventListener('resize', function(event) {
+  let width = window.innerWidth;
+  if (width <= 648) {
+    $("#popover-additional-btn-h").hide();
+    $("#popover-additional-btn-c").show();
+  } else {
+    $("#popover-additional-btn-c").hide();
+    $("#popover-additional-btn-h").show();
+  }
+  
+  // popover-additional-btn
+}, true);
+$("#popover-additional-close").click(function () {
+  $("#popover-additional").hide();
+})
+
+$("#popover-additional-btn-h").hover(function () {
+  $("#popover-additional").show();
+})
+$("#popover-additional-btn-c").click(function () {
+  $("#popover-additional").show();
+})
+
 
 // export
-export default Footer;
+export default PropertyCard;
 
 // initiate
-Footer.init();
+PropertyCard.init();
